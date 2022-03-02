82047393b59755f8a483678c02a1f1fc5f442c99
Dũng Bùi <88707213+dungbui0304@users.noreply.github.com>
2022-03-02 21:00:25 +0700

    Add files via upload
    up code trang chủ, đăng kí, đăng nhập

---
+++ login.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>MoneyLover</title>
+    <link rel="stylesheet" href="./assest/css/style.css">
+    <link rel="stylesheet" href="./assest/fonts/fontawesome-free-5.15.4/css/all.min.css">
+</head>
+
+<body>
+    <div class="main__login">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <svg data-v-0624972a="" xmlns="http://www.w3.org/2000/svg" width="230" height="130"
+                        viewBox="0 0 230 130" class="logo-icon logo-desktop">
+                        <g id="Group_883" data-name="Group 883" transform="translate(-598 -30)">
+                            <g id="Group_3" data-name="Group 3" transform="translate(615 32)">
+                                <g id="Group_1" data-name="Group 1" transform="translate(0 96.263)">
+                                    <path id="Path_1" data-name="Path 1"
+                                        d="M97.021,39.229c-.758,3.032-3.032,3.79-6.064,3.79a5.79,5.79,0,0,1-3.79-1.516l1.516-3.032a2.785,2.785,0,0,0,2.274.758c1.516,0,2.274-.758,3.032-2.274l.758-1.516-7.58-17.433H90.2a2.348,2.348,0,0,1,1.516.758c1.516,3.032,5.306,12.128,5.306,12.128s3.79-9.1,4.548-12.128c0-.758,1.516-.758,1.516-.758h3.032ZM78.071,21.8c-3.032,0-4.548,1.516-5.306,4.548H83.377C82.619,23.312,81.1,21.8,78.071,21.8Zm8.338,7.58H72.766a5.537,5.537,0,0,0,5.306,4.548,6.881,6.881,0,0,0,5.306-2.274l2.274,2.274a11.58,11.58,0,0,1-7.58,3.032,8.757,8.757,0,0,1-9.1-9.1,8.757,8.757,0,0,1,9.1-9.1C84.135,18.764,87.167,23.312,86.409,29.375ZM62.154,36.2V26.344a3.581,3.581,0,0,0-3.79-3.79c-3.032,0-5.306,2.274-5.306,4.548v9.1h-3.79V19.522l3.79-.758v3.79a7.96,7.96,0,0,1,6.822-3.79q6.822,0,6.822,6.822V36.2H62.154ZM37.141,22.554a5.181,5.181,0,0,0-5.306,5.306c0,3.032,2.274,6.064,5.306,6.064s5.306-2.274,5.306-6.064A5.181,5.181,0,0,0,37.141,22.554Zm0,14.4a8.757,8.757,0,0,1-9.1-9.1,8.757,8.757,0,0,1,9.1-9.1,8.757,8.757,0,0,1,9.1,9.1A8.757,8.757,0,0,1,37.141,36.955ZM25.013,36.2h-3.79V19.522L14.4,33.165a2.348,2.348,0,0,1-1.516.758h-.758a2.348,2.348,0,0,1-1.516-.758L3.79,19.522V36.2H0V12.7H3.79a2.348,2.348,0,0,1,1.516.758c1.516,3.79,7.58,14.4,7.58,14.4s6.064-11.37,7.58-14.4a2.348,2.348,0,0,1,1.516-.758h3.79Z"
+                                        transform="translate(0 -12.7)" fill="#fff"></path>
+                                    <path id="Path_2" data-name="Path 2"
+                                        d="M89.239,27.2v9.1H85.45V19.622l3.79-.758v3.79c1.516-2.274,3.032-3.79,6.064-3.79V21.9C91.513,21.9,89.239,24.17,89.239,27.2ZM74.08,21.138c-3.032,0-4.548,1.516-5.306,4.548H79.386C78.628,23.412,77.112,21.138,74.08,21.138Zm8.338,7.58H68.774a5.537,5.537,0,0,0,5.306,4.548,6.881,6.881,0,0,0,5.306-2.274l2.274,2.274A11.58,11.58,0,0,1,74.08,36.3a8.757,8.757,0,0,1-9.1-9.1,8.757,8.757,0,0,1,9.1-9.1C80.144,18.106,83.176,22.654,82.418,28.717ZM58.163,36.3h-3.79L47.551,18.864h3.032a2.348,2.348,0,0,1,1.516.758c1.516,3.032,4.548,12.886,4.548,12.886s3.79-9.854,4.548-12.886c0-.758,1.516-.758,1.516-.758h3.032ZM37.7,21.9A5.181,5.181,0,0,0,32.391,27.2c0,3.032,2.274,6.064,5.306,6.064S43,30.991,43,27.2A5.181,5.181,0,0,0,37.7,21.9Zm0,14.4a8.757,8.757,0,0,1-9.1-9.1,8.757,8.757,0,0,1,9.1-9.1,8.757,8.757,0,0,1,9.1,9.1A8.757,8.757,0,0,1,37.7,36.3Zm-23.5-.758V12.8h3.032a.742.742,0,0,1,.758.758V32.507h8.338a.742.742,0,0,1,.758.758v2.274Z"
+                                        transform="translate(101.291 -12.042)" fill="#fff"></path>
+                                </g>
+                                <g id="Group_2" data-name="Group 2" transform="translate(61.995)">
+                                    <path id="Path_3" data-name="Path 3"
+                                        d="M26.549.1H61.416A18.765,18.765,0,0,1,80.366,19.049V53.916A18.445,18.445,0,0,1,62.174,72.108H25.791A18.445,18.445,0,0,1,7.6,53.916V19.049A18.765,18.765,0,0,1,26.549.1Z"
+                                        transform="translate(-7.6 0.658)" fill="#2db84c" fill-rule="evenodd"></path>
+                                    <path id="Path_4" data-name="Path 4"
+                                        d="M25.791,0H62.174A18.445,18.445,0,0,1,80.366,18.191v7.58A18.445,18.445,0,0,1,62.174,43.963H25.791A18.445,18.445,0,0,1,7.6,25.771v-7.58A18.445,18.445,0,0,1,25.791,0Z"
+                                        transform="translate(-7.6 0)" fill="#e2ece4" fill-rule="evenodd"></path>
+                                    <path id="Path_5" data-name="Path 5"
+                                        d="M13.29,6.3a3.581,3.581,0,0,1,3.79,3.79,3.581,3.581,0,0,1-3.79,3.79c-1.516,0-3.79-2.274-3.79-3.79A4.06,4.06,0,0,1,13.29,6.3Z"
+                                        transform="translate(4.901 41.452)" fill="#247936" fill-rule="evenodd"></path>
+                                    <path id="Path_6" data-name="Path 6"
+                                        d="M22.37,6.8c6.064,0,11.37,3.79,11.37,9.1s-5.306,9.1-11.37,9.1S11,21.2,11,15.9,16.306,6.8,22.37,6.8Z"
+                                        transform="translate(14.771 44.742)" fill="#70d185" fill-rule="evenodd"></path>
+                                    <path id="Path_7" data-name="Path 7"
+                                        d="M12.631,7.7A2.42,2.42,0,0,1,14.9,9.974a1.63,1.63,0,0,1-1.516,1.516,1.63,1.63,0,0,1-1.516-1.516A1.452,1.452,0,0,1,12.631,7.7Z"
+                                        transform="translate(18.446 50.664)" fill="#009421" fill-rule="evenodd"></path>
+                                    <path id="Path_8" data-name="Path 8"
+                                        d="M11.874,3.9H45.225a1.63,1.63,0,0,1,1.516,1.516,1.63,1.63,0,0,1-1.516,1.516H11.874a1.63,1.63,0,0,1-1.516-1.516C9.6,4.658,10.358,3.9,11.874,3.9Z"
+                                        transform="translate(8.591 25.661)" fill="#006015" fill-rule="evenodd"></path>
+                                    <path id="Path_9" data-name="Path 9"
+                                        d="M25.66,1a14.392,14.392,0,0,1,14.4,14.4,15.357,15.357,0,0,1-4.548,10.612H15.806c-3.032-3.032-5.306-6.822-5.306-11.37C10.5,7.064,17.322,1,25.66,1Z"
+                                        transform="translate(11.481 6.58)" fill="#ffce00" fill-rule="evenodd"></path>
+                                    <path id="Path_10" data-name="Path 10"
+                                        d="M20.038,1.5V20.449H14.732V10.6c0-1.516-.758-.758-.758-.758H11.7V9.08a53.1,53.1,0,0,0,7.58-6.064C19.28,1.5,20.038,1.5,20.038,1.5Z"
+                                        transform="translate(19.377 9.87)" fill="#fff" fill-rule="evenodd"></path>
+                                    <path id="Path_11" data-name="Path 11"
+                                        d="M14.774,7.7A1.63,1.63,0,0,1,16.29,9.216a1.63,1.63,0,0,1-1.516,1.516,1.63,1.63,0,0,1-1.516-1.516C12.5,9.216,13.258,7.7,14.774,7.7Z"
+                                        transform="translate(27.672 50.664)" fill="#009421" fill-rule="evenodd"></path>
+                                    <path id="Path_12" data-name="Path 12"
+                                        d="M18.19,6.3a3.581,3.581,0,0,1,3.79,3.79,3.581,3.581,0,0,1-3.79,3.79,3.581,3.581,0,0,1-3.79-3.79A3.581,3.581,0,0,1,18.19,6.3Z"
+                                        transform="translate(37.142 41.452)" fill="#247936" fill-rule="evenodd"></path>
+                                </g>
+                            </g>
+                            <rect id="Rectangle_329" data-name="Rectangle 329" width="230" height="130"
+                            transform="translate(598 30)" fill="none"></rect>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="wrap__login">
+        <div class="container__login">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="wrap__form">
+                        <div class="form__title-text">
+                            <h2>Log In</h2>
+                        </div>
+                        <div class="form__body">
+                            <div class="socials flex">
+                                <div class="socials__des">
+                                    <span>Using social networking accounts</span>
+                                </div>
+                                <div class="socials__item">
+                                    <button class="socials__item-btn gg">
+                                        <svg data-v-8cacc26a="" id="Group_818" data-name="Group 818"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                            class="social-item-icon">
+                                            <g id="Group_862" data-name="Group 862">
+                                                <rect id="Rectangle_326" data-name="Rectangle 326" width="24" height="24"
+                                                    fill="none"></rect>
+                                                <path id="Path_3814" data-name="Path 3814"
+                                                    d="M261.529,3012.3a5.143,5.143,0,0,1,3.551,1.34l2.592-2.481a8.919,8.919,0,0,0-6.143-2.339,9.2,9.2,0,0,0-8.2,4.959l2.969,2.26a5.54,5.54,0,0,1,5.231-3.739"
+                                                    transform="translate(-249.345 -3005.818)" fill="#ff3e3e"
+                                                    fill-rule="evenodd"></path>
+                                                <path id="Path_3815" data-name="Path 3815"
+                                                    d="M270.345,3018.018a7.545,7.545,0,0,0-.194-1.84h-8.622v3.34h5.061a4.388,4.388,0,0,1-1.878,2.92l2.9,2.2a8.712,8.712,0,0,0,2.733-6.62"
+                                                    transform="translate(-249.345 -3005.818)" fill="#3b8aff"
+                                                    fill-rule="evenodd"></path>
+                                                <path id="Path_3816" data-name="Path 3816"
+                                                    d="M256.3,3019.6a5.423,5.423,0,0,1-.306-1.78,5.712,5.712,0,0,1,.3-1.781l-2.969-2.259a8.817,8.817,0,0,0,0,8.08Z"
+                                                    transform="translate(-249.345 -3005.818)" fill="#ffce12"
+                                                    fill-rule="evenodd"></path>
+                                                <path id="Path_3817" data-name="Path 3817"
+                                                    d="M261.529,3026.818a8.858,8.858,0,0,0,6.081-2.18l-2.9-2.2a5.677,5.677,0,0,1-8.408-2.84l-2.969,2.26a9.182,9.182,0,0,0,8.2,4.96"
+                                                    transform="translate(-249.345 -3005.818)" fill="#27b648"
+                                                    fill-rule="evenodd"></path>
+                                            </g>
+                                        </svg>
+                                        <span class="socials__connect">Connect with Google</span>
+                                    </button>
+                                    <button class="socials__item-btn fb">
+                                        <svg data-v-edfbec8c="" id="Group_817" data-name="Group 817"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                            class="social-item-icon">
+                                            <g id="Group_816" data-name="Group 816">
+                                                <g id="Group_786" data-name="Group 786" transform="translate(2.728 2.881)">
+                                                    <path id="Exclusion_1" data-name="Exclusion 1"
+                                                        d="M5776.945-352.489h-2.015a6.422,6.422,0,0,1-3.4-.59,3.99,3.99,0,0,1-1.669-1.667,6.448,6.448,0,0,1-.589-3.4v-7.075a6.44,6.44,0,0,1,.589-3.4,4.014,4.014,0,0,1,1.669-1.669,6.441,6.441,0,0,1,3.4-.588H5782a6.438,6.438,0,0,1,3.4.588,4.015,4.015,0,0,1,1.669,1.669,6.425,6.425,0,0,1,.59,3.4v7.075a6.433,6.433,0,0,1-.59,3.4,3.991,3.991,0,0,1-1.669,1.667,6.419,6.419,0,0,1-3.4.59h-2.144v-6.626h2.179l.407-2.689h-2.584V-363.6a1.4,1.4,0,0,1,1.4-1.4h1.28v-2.282a12.034,12.034,0,0,0-1.975-.194c-.2,0-.391.009-.56.025a3.223,3.223,0,0,0-2.9,2.346,5.7,5.7,0,0,0-.135,1.5c.016.43.007,1.383,0,1.773v.021H5774.6v2.689h2.341v6.626Z"
+                                                        transform="translate(-5769.272 370.881)" fill="#1877f2"
+                                                        stroke="rgba(0,0,0,0)" stroke-miterlimit="10" stroke-width="1">
+                                                    </path>
+                                                </g>
+                                                <rect id="Rectangle_308" data-name="Rectangle 308" width="24" height="24"
+                                                    fill="none"></rect>
+                                            </g>
+                                        </svg>
+                                        <span class="socials__connect">Connect with Facebook</span>
+                                    </button>
+                                    <button class="socials__item-btn ip">
+                                        <svg id="Group_788" data-name="Group 788" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" viewBox="0 0 24 24" class="social-item-icon">
+                                            <rect id="Rectangle_308" data-name="Rectangle 308" width="24" height="24"
+                                                fill="none"></rect>
+                                            <g id="Group_781" data-name="Group 781" transform="translate(3 1.589)">
+                                                <rect id="Rectangle_305" data-name="Rectangle 305" width="19" height="19"
+                                                    transform="translate(0 0.411)" fill="none"></rect>
+                                                <path id="Path_3806" data-name="Path 3806"
+                                                    d="M18.67,8.385a4.24,4.24,0,0,0-2.037,3.579,4.148,4.148,0,0,0,2.518,3.81,9.71,9.71,0,0,1-1.285,2.675c-.8,1.157-1.651,2.328-2.93,2.328s-1.614-.759-3.086-.759c-1.446,0-1.952.782-3.121.782S6.74,19.717,5.8,18.4a11.583,11.583,0,0,1-1.965-6.243c0-3.653,2.374-5.607,4.725-5.607,1.243,0,2.285.82,3.062.82.736,0,1.892-.868,3.315-.868A4.426,4.426,0,0,1,18.67,8.385M11.715,6.3a1.542,1.542,0,0,1-.253-.023,1.718,1.718,0,0,1-.037-.34,4.093,4.093,0,0,1,1.036-2.555,4.183,4.183,0,0,1,2.76-1.429,1.8,1.8,0,0,1,.037.375,4.256,4.256,0,0,1-.988,2.628A3.659,3.659,0,0,1,11.715,6.3Z"
+                                                    transform="translate(-2.678 -1.952)"></path>
+                                            </g>
+                                        </svg>
+                                        <span class="socials__connect">Sign in with Apple</span>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="account flex">
+                                <div class="account__des">
+                                    <span>Using Money Lover account</span>
+                                </div>
+                                <form action="" class="form">
+                                    <input class="input__text" type="text" placeholder="Email">
+                                    <input class="input__text" type="text" placeholder="Password">
+                                    <div class="form__forgot">
+                                        <a href="" class="form__forgot-link">Forgot Password?</a>
+                                    </div>
+                                    <button class="form__btn">LOGIN</button>
+                                </form>
+                                <div class="form__suggest">
+                                    <p class="form__suggest-text">Don’t have an account?
+                                        <a href="register.php" class="form__suggest-text-link">Register</a>
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer__login">
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
