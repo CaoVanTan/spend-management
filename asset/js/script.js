84fe478c4f74fbd546047c249a54b780935b2c4e
NguyenManh <manh95405@gmail.com>
2022-03-04 16:20:31 +0700

    Update code

---
+++ asset/js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    $(".jsthistmonth").click(function() {
+        $(".thisMonth").show();
+        $(".main").hide();
+    });
+    $(".jslastmonth").click(function() {
+        $(".thisMonth").hide();
+        $(".main").show();
+    });
+    $(".jsFuture").click(function() {
+        $(".thisMonth").hide();
+        $(".main").show();
+    });
+    $(".list-transaction").click(function() {
+        $(".js_detail").show();
+        $(".jscontent-main").css({ marginRight: "370px" });
+    });
+    $(".jsbtnedittran").click(function() {
+        $(".Edit-transaction").show();
+    });
+    $(".jsicon_detail").click(function() {
+        $(".js_detail").hide();
+        $(".jscontent-main").css({ marginRight: "-40px" });
+    });
+    $(".jsbtndletetran").click(function() {
+        $(".delete-transaction").show();
+    });
+    $(".jsbtnNodelete").click(function() {
+        $(".delete-transaction").hide();
+    });
+    $(".jsbtnaddtran").click(function() {
+        $(".transaction").show();
+    });
+    $(".jsbtntran").click(function() {
+        $(".transaction").hide();
+        $(".Edit-transaction").hide();
+    });
+    $(".jscategory").click(function() {
+        $(".category").show();
+    });
+    $(".jsIcon").click(function() {
+        $(".category").hide();
+    });
+    // Model add
+    $(".jsExpense").click(function() {
+        $(".list-expense").show();
+        $(".list-debt").hide();
+        $(".list-income").hide();
+    });
+    $(".jsDebt").click(function() {
+        $(".list-expense").hide();
+        $(".list-debt").show();
+        $(".list-income").hide();
+    });
+    $(".jsIncome").click(function() {
+        $(".list-expense").hide();
+        $(".list-debt").hide();
+        $(".list-income").show();
+    });
+
+    // $(".btnCancelAdd").click(function() {
+    //     $(".overlay").hide();
+    //     $(".model").hide();
+    // });
+    // //model find
+    // $(".searc").click(function() {
+    //     $(".find").show();
+    // });
+    // $(".find-close").click(function() {
+    //     $(".find").hide();
+    // });
+    // // Process Login
+    // $("#btnLogin").click(function() {
+    //     var userName = $("#inputUserName").val();
+    //     var password = $("#inputPassword").val();
+
+    //     if (userName == "" || password == "") {
+    //         alert("Bạn phải nhập tài khoản và mật khẩu!");
+    //     }
+    // });
+
+});
