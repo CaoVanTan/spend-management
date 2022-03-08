39759a4d37564999bd21193ffc46df0aa8a732fe
PhongGT <megayako9@gmail.com>
2022-03-08 23:04:18 +0700

    fix login

--- process/login.php
+++ process/login.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
-    $username =$_POST["username"];
-    $pass =$_POST["pass"];
+    $username =$_POST["Username"];
+    $pass = $_POST["Pass"];
     include('../config/config.php');
 
-    $sql ="Select * From users where user_name = ?";
-    $result = mysqli_query($conn,$sql);
+    $sql ="Select * From users where user_name = '$username'";
+    $result = mysqli_query($con,$sql);
     if(mysqli_num_rows($result) > 0){
             $row = mysqli_fetch_assoc($result);
             $userid = $row['user_id'];
@@ -16,11 +16,11 @@
                 $_SESSION['CurrentID'] = $userid;
                 echo "Thanhcong";
             }else {
-                mysqli_close($conn);
+                mysqli_close($con);
                 echo "Sai";
             }
     }else {
-        mysqli_close($conn);
+        mysqli_close($con);
         echo "Sai";
     }
 ?>
