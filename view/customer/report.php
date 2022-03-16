5affcead78a073108511e30577b801f97b3483f2
PhongGT <megayako9@gmail.com>
2022-03-16 22:22:09 +0700

    xu ly bieu do thong ke

--- view/customer/report.php
+++ view/customer/report.php
@@ -1,5 +1,35 @@
 <?php
 include_once '../../partials-front/header.php';
+include_once '../../config/config.php';
+    $Ngay = array();
+    $TongTien = array();
+    $Songay = date("d", mktime(0, 0, 0, date("m")+1, 0, date("Y")));
+    $dem = 1;
+    $sqlSumMoney = "SELECT sum(money) as 'Tong', Day(spend_day) as 'Day' FROM spending sp, groups g, users u where 
+    sp.group_id = g.group_id AND sp.user_id = u.user_id AND u.user_name = 'cvtan' AND sp.spend_day like '2022-03%' group by spend_day";
+    $resultSumMoney = mysqli_query($con, $sqlSumMoney);
+    if (mysqli_num_rows($resultSumMoney) > 0) { 
+        while ($row =   mysqli_fetch_assoc($resultSumMoney)){  
+            for ($i= $dem; $i <= $row['Day'] ; $i++) { 
+                $Ngay[$i-1] = $i;
+                if ($i == $row['Day']) {
+                    $TongTien[$i-1] = $row['Tong'];
+                    $dem = $row['Day']+1;
+                    echo $dem;
+                }
+                else {
+                    $TongTien [$i-1] = 0;
+                }
+            }
+        }
+        for ($i=$dem; $i<=$Songay ; $i++) {
+            $TongTien [$i-1] = 0;
+            $Ngay[$i-1] = $i;
+        }
+    }
+    print_r($TongTien);
+    echo '</br>';
+    print_r($Ngay);
 ?>
 <section>
     <div class="container">
@@ -8,33 +38,33 @@ include_once '../../partials-front/header.php';
         ?>
     </div>
 
-    <?php
-    include_once '../../partials-front/header_transaction.php';
-    ?>
+
 
     <div class="main__report">
         <div class="report">
             <div class="report__top flex">
                 <div class="report__text">
-                    <h5>Thu nhập ròng</h5>
+                    <h5>Thu nhập</h5>
                 </div>
             </div>
             <div class="report__chart">
                 <canvas id="myChart" style="width:80%; padding:68px"></canvas>
                 <script>
-                    var xValues = ["1", "3", "5", "7", "9", "11", "13", "15", "17", "19", "21", "23", "25", "27", "29",
+                    var xValues = ["1","2", "3", "5", "7", "9", "11", "13", "15", "17", "19", "21", "23", "25", "27", "29",
                         "31"
                     ];
                     var Moneys = [0, 200, 400, 800, 1000];
-                    var Colors = ["red", "green", "blue", "orange", "brown"];
+                    var Colors = ["red", "green"];
 
                     new Chart("myChart", {
                         type: "bar",
-                        data: {
-                            labels: xValues,
+                        data: { 
+                            labels: <?php echo json_encode($Ngay)?>,
+                            yValueFormatString: "#,##0.## đồng",
                             datasets: [{
-                                backgroundColor: Colors,
-                                data: Moneys
+                               
+                                backgroundColor: '#45F143',
+                                data: <?php echo json_encode($TongTien)?>,
                             }]
                         },
                         options: {
@@ -81,15 +111,7 @@ include_once '../../partials-front/header.php';
                 <div class="modal-title">
                     <div class="modal-wrap1 flex">
                         <div class="modal-label">
-                            <p>Dòng vào</p>
-                        </div>
-                        <div class="modal-money">
-                            <p style="color: blue;">0 đ</p>
-                        </div>
-                    </div>
-                    <div class="modal-wrap1 flex">
-                        <div class="modal-label">
-                            <p>Chảy ra</p>
+                            <p>Chi tiêu</p>
                         </div>
                         <div class="modal-money color">
                             <p>-2.00 đ</p>
